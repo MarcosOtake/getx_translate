676b8facd6f617f8ff5a5509dfa10be355019df5
Marcos Otake <ext_marcos.otake@cargobr.com>
2022-11-16 22:48:08 -0300

    configuração de traduções

---
+++ lib/core/translation/translation_keys.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+
+class TranslationKeys extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+        'en_US': {
+          'title_app': 'GetX Translate',
+        },
+        'pt_BR': {
+          'title_app': 'GetX Tradução',
+        },
+        'jp_JP': {
+          'title_app': 'GetX 翻訳 - Hon\'yaku',
+        }
+      };
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_translate/core/translation/translation_keys.dart';
+import 'package:getx_translate/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,59 +12,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      translations: TranslationKeys(),
+      locale: const Locale('pt', 'BR'),
+      //locale: const Locale('en', 'US'),
+      //locale: const Locale('jp', 'JP'),
+      fallbackLocale: const Locale('pt', 'BR'),
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      getPages: [
+        GetPage(name: '/', page: () => const HomePage()),
+      ],
     );
   }
 }
